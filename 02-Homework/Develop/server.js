cddb8663e8d2f2473982f487b1ff569b43040153
Qabas al ani <doctorqa@yahoo.com>
2021-08-31 19:48:07 -0400

    added README file

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const express = require("express");
+const routes = require("./routes");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,5 +13,5 @@ app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
